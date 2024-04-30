55a493336f249d00d776ea1c65072e973db6bad1
Kayck10 <kaycktarcisio@gmail.com>
2024-04-30 00:04:35 -0300

    atualiza

--- app/Http/Controllers/AtendimentoEscolasController.php
+++ app/Http/Controllers/AtendimentoEscolasController.php
@@ -8,12 +8,37 @@ use Illuminate\Http\Request;
 
 class AtendimentoEscolasController extends Controller
 {
-    public function index () {
+    public function index()
+    {
         $escolas = Local::all();
         return view('atendimento-escolas.index', compact('escolas'));
     }
 
-    public function store (Request $request) {
-        return response()->json(['error' => false, 'message' => 'Atendimento cadastro com sucesso!', 'dados-infomardo' => $request->all()]);
+    public function store(Request $request)
+    {
+        $request->validate([
+            'id_local' => 'required|exists:local,id',
+            'externo' => 'required|boolean',
+        ]);
+        $local = Local::find($request->input('id_local'));
+        if ($local->nome === 'Secretaria de Educacao') {
+            if ($request->input('externo')) {
+                return response()->json([
+                    'message' => 'Para Secretaria de Educacao, "externo" não deve ser marcado'
+                ], 400);
+            }
+        } else {
+            if (!$request->input('externo')) {
+                return response()->json([
+                    'message' => ''
+                ], 400);
+            }
+        }
+        $atendimento = Atendimentos::create([
+            'id_local' => $request->input('id_local'),
+            'externo' => $request->input('externo'),
+        ]);
+
+        return response()->json($atendimento, 201);
     }
 }
--- database/migrations/2024_04_08_114256_create_atendimentos_table.php
+++ database/migrations/2024_04_08_114256_create_atendimentos_table.php
@@ -11,21 +11,21 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('atendimentos', function (Blueprint $table) {
-            $table->id();
-            $table->unsignedBigInteger('id_local');
-            $table->foreign('id_local')->references('id')->on('local');
-            $table->unsignedBigInteger('id_equipamento');
-            $table->foreign('id_equipamento')->references('id')->on('equipamentos');
-            $table->unsignedBigInteger('id_status');
-            $table->foreign('id_status')->references('id')->on('status_atendimentos');
-            $table->unsignedBigInteger('id_user');
-            $table->foreign('id_user')->references('id')->on('users');
-            $table->text('desc_problema');
-            $table->boolean('prioridade');
-            $table->boolean('externo');
-            $table->timestamps();
-        });
+            Schema::create('atendimentos', function (Blueprint $table) {
+                $table->id();
+                $table->unsignedBigInteger('id_local');
+                $table->foreign('id_local')->references('id')->on('local');
+                $table->unsignedBigInteger('id_equipamento')->nullable();
+                $table->foreign('id_equipamento')->references('id')->on('equipamentos');
+                $table->unsignedBigInteger('id_status')->default(1);
+                $table->foreign('id_status')->references('id')->on('status_atendimentos');
+                $table->unsignedBigInteger('id_user');
+                $table->foreign('id_user')->references('id')->on('users');
+                $table->text('desc_problema')->nullable();
+                $table->boolean('prioridade')->default(false);
+                $table->boolean('externo')->default(false);
+                $table->timestamps();
+            });
     }
 
     /**
