3d2de1c4fad68df143cb9f9214ba318bff41afac
Kayck10 <kaycktarcisio@gmail.com>
2024-09-20 08:24:20 -0300

    subindo

--- config/app.php
+++ config/app.php
@@ -9,8 +9,9 @@ return [
     | Application Name
     |--------------------------------------------------------------------------
     |
-    | The name of your application. This will be used when the application needs
-    | to present itself to the user or when building notifications.
+    | This value is the name of your application. This value is used when the
+    | framework needs to place the application's name in a notification or
+    | any other location as required by the application or its packages.
     |
     */
 
@@ -21,9 +22,9 @@ return [
     | Application Environment
     |--------------------------------------------------------------------------
     |
-    | The environment your application is running in. This could be local, staging,
-    | production, etc. This configuration value determines how the application
-    | behaves and which settings it uses.
+    | This value determines the "environment" your application is currently
+    | running in. This may determine how you prefer to configure various
+    | services the application utilizes. Set this in your ".env" file.
     |
     */
 
@@ -34,9 +35,9 @@ return [
     | Application Debug Mode
     |--------------------------------------------------------------------------
     |
-    | When your application is in debug mode, detailed error messages with stack
-    | traces will be shown on every error that occurs within your application.
-    | Otherwise, a simple generic error page is shown.
+    | When your application is in debug mode, detailed error messages with
+    | stack traces will be shown on every error that occurs within your
+    | application. If disabled, a simple generic error page is shown.
     |
     */
 
@@ -47,31 +48,38 @@ return [
     | Application URL
     |--------------------------------------------------------------------------
     |
-    | This URL is used by the console to generate URLs when using the Artisan command-line tool.
-    | You should set this to the root of your application so that it is used when generating URLs.
+    | This URL is used by the console to properly generate URLs when using
+    | the Artisan command line tool. You should set this to the root of
+    | your application so that it is used when running Artisan tasks.
     |
     */
 
     'url' => env('APP_URL', 'http://localhost'),
+
+    'asset_url' => env('ASSET_URL', '/'),
 
     /*
     |--------------------------------------------------------------------------
     | Application Timezone
     |--------------------------------------------------------------------------
     |
-    | Here you may specify the default timezone for your application. This will
-    | be used by the PHP date and date-time functions.
+    | Here you may specify the default timezone for your application, which
+    | will be used by the PHP date and date-time functions. We have gone
+    | ahead and set this to a sensible default for you out of the box.
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'America/Sao_Paulo',
+
 
     /*
     |--------------------------------------------------------------------------
     | Application Locale Configuration
     |--------------------------------------------------------------------------
     |
-    | The locale determines the default locale that will be used by the translation service provider.
+    | The application locale determines the default locale that will be used
+    | by the translation service provider. You are free to set this value
+    | to any of the locales which will be supported by the application.
     |
     */
 
@@ -82,7 +90,9 @@ return [
     | Application Fallback Locale
     |--------------------------------------------------------------------------
     |
-    | The fallback locale is used when the current locale is not available.
+    | The fallback locale determines the locale to use when the current one
+    | is not available. You may change the value to correspond to any of
+    | the language folders that are provided through your application.
     |
     */
 
@@ -93,7 +103,9 @@ return [
     | Faker Locale
     |--------------------------------------------------------------------------
     |
-    | The locale that Faker should use when generating fake data.
+    | This locale will be used by the Faker PHP library when generating fake
+    | data for your database seeds. For example, this will be used to get
+    | localized telephone numbers, street address information and more.
     |
     */
 
@@ -104,9 +116,9 @@ return [
     | Encryption Key
     |--------------------------------------------------------------------------
     |
-    | This key is used by the Laravel encrypter service and should be set to a
-    | random, 32 character string, otherwise these encrypted strings will not
-    | be safe. Please do this before deploying an application!
+    | This key is used by the Illuminate encrypter service and should be set
+    | to a random, 32 character string, otherwise these encrypted strings
+    | will not be safe. Please do this before deploying an application!
     |
     */
 
@@ -116,22 +128,44 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Service Providers
+    | Maintenance Mode Driver
     |--------------------------------------------------------------------------
     |
-    | Here you may list the service providers for your application. These are
-    | loaded automatically by the framework. You can add your own providers here.
+    | These configuration options determine the driver used to determine and
+    | manage Laravel's "maintenance mode" status. The "cache" driver will
+    | allow maintenance mode to be controlled across multiple machines.
+    |
+    | Supported drivers: "file", "cache"
+    |
+    */
+
+    'maintenance' => [
+        'driver' => 'file',
+        // 'store'  => 'redis',
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Autoloaded Service Providers
+    |--------------------------------------------------------------------------
+    |
+    | The service providers listed here will be automatically loaded on the
+    | request to your application. Feel free to add your own services to
+    | this array to grant expanded functionality to your applications.
     |
     */
 
     'providers' => [
+
         /*
          * Laravel Framework Service Providers...
          */
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
         Illuminate\Bus\BusServiceProvider::class,
+        Brian2694\Toastr\ToastrServiceProvider::class,
         Illuminate\Cache\CacheServiceProvider::class,
+        Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class,
         Illuminate\Cookie\CookieServiceProvider::class,
         Illuminate\Database\DatabaseServiceProvider::class,
         Illuminate\Encryption\EncryptionServiceProvider::class,
@@ -149,11 +183,12 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Barryvdh\DomPDF\ServiceProvider::class,
+
 
         /*
          * Package Service Providers...
          */
-        Barryvdh\DomPDF\ServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -163,6 +198,8 @@ return [
         App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
+
     ],
 
     /*
@@ -170,14 +207,19 @@ return [
     | Class Aliases
     |--------------------------------------------------------------------------
     |
-    | Here you may list all of the class aliases that should be registered for
-    | your application. These are loaded automatically and are used to provide
-    | a more convenient way of accessing classes throughout your application.
+    | This array of class aliases will be registered when this application
+    | is started. However, feel free to register as many as you wish as
+    | the aliases are "lazy" loaded so they don't hinder performance.
     |
     */
 
     'aliases' => Facade::defaultAliases()->merge([
-        'PDF' => Barryvdh\DomPDF\Facade\Pdf::class,
+        // 'ExampleClass' => App\Example\ExampleClass::class,
     ])->toArray(),
+    'aliases' => [
+        // Outros aliases...
+        'PDF' => Barryvdh\DomPDF\Facade\Pdf::class,
+    ],
+
 
 ];
